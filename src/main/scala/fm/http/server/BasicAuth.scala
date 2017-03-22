ac55904ce0f6079f522c87a2ccc9991a87ce85aa
Tim Underwood <timunderwood@gmail.com>
2017-03-22 13:40:40 -0700

    BasicAuth tweaks for previous commit

--- src/main/scala/fm/http/server/BasicAuth.scala
+++ src/main/scala/fm/http/server/BasicAuth.scala
@@ -15,24 +15,19 @@
  */
 package fm.http.server
 
-import fm.common.{Base64, Logging}
+import fm.common.Logging
 import fm.common.Implicits._
 import fm.http.{Headers, Status}
-import java.nio.charset.StandardCharsets
 import io.netty.handler.codec.http.HttpHeaders
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.matching.Regex
 
 object BasicAuth {
-  private val BasicAuthHeader: Regex = """Basic (.+)""".r
-  private val BasicAuthSplit: Regex = """(.+):(.+)""".r
-
   private final case class StaticUsersBasicAuth (realm: String, users: Map[String, String]) extends BasicAuth {
     def isAuthorized(user: String, pass: String): Future[Boolean] = Future.successful(users.get(user).map{ _ === pass }.getOrElse(false))
   }
 
-  // Performs Basic Authentication using the given Map of Username -> Plaintext Password before calling the passed in handler
+  /** Performs Basic Authentication using the given Map of Username -> Plaintext Password before calling the passed in handler */
   def apply(realm: String, users: Map[String, String]): BasicAuth = StaticUsersBasicAuth(realm, users)
 }
 
@@ -41,31 +36,25 @@ object BasicAuth {
  * http://en.wikipedia.org/wiki/Basic_access_authentication
  */
 trait BasicAuth extends Auth with Logging {
-  import BasicAuth._
-
   // Implemented by child classes
-  protected def realm: String
-  protected def isAuthorized(user: String, pass: String): Future[Boolean]
+  def realm: String
+  def isAuthorized(user: String, pass: String): Future[Boolean]
 
-  protected def requireAuthImpl(request: Request)(action: => Future[Response]): Future[Response] = {
-    isValid(request).flatMap{ valid: Boolean =>
+  final protected def requireAuthImpl(request: Request)(action: => Future[Response]): Future[Response] = {
+    isAuthorized(request).flatMap{ valid: Boolean =>
       if (valid) action
       else Future.successful(Response(Status.UNAUTHORIZED, Headers(HttpHeaders.Names.WWW_AUTHENTICATE -> s"""Basic realm="$realm"""")))
     }
   }
 
-  private def isValid(request: Request): Future[Boolean] = try {
+  final def isAuthorized(request: Request): Future[Boolean] = try {
     val auth: String = request.headers.authorization.getOrElse{ return Future.successful(false) }
 
-    auth match {
-      case BasicAuthHeader(encoded) =>
-        new String(Base64.decode(encoded), StandardCharsets.ISO_8859_1) match {
-          case BasicAuthSplit(user, pass) => isAuthorized(user, pass)
-          case _ => Future.successful(false)
-        }
-
-      case _ => Future.successful(false)
+    Headers.parseBasicAuthorization(auth) match {
+      case Some((user,pass)) => isAuthorized(user, pass)
+      case None => Future.successful(false)
     }
+
   } catch {
     case ex: Exception =>
       logger.warn("Caught Exception Performing Basic Auth", ex)
