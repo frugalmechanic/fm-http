89b756e0cd8908581c454a3a9d68cd8743ce3c0a
Tim Underwood <timunderwood@gmail.com>
2017-11-20 12:48:51 -0800

    Do not attempt to double send a ByteBuf in AuthHttpClient otherwise we get "io.netty.util.IllegalReferenceCountException: refCnt: 0, decrement: 1"

--- src/main/scala/fm/http/client/AuthHttpClient.scala
+++ src/main/scala/fm/http/client/AuthHttpClient.scala
@@ -1,6 +1,8 @@
 package fm.http.client
 
+import fm.common.Implicits._
 import fm.http.MutableHeaders
+import io.netty.buffer.ByteBuf
 import scala.concurrent.Future
 import scala.concurrent.duration.Duration
 
@@ -22,21 +24,44 @@ abstract class AuthHttpClient extends HttpClientProxy {
   protected def makeAuthorization(request: Request, response: Response): Option[String]
   
   final override def execute(request: Request, timeout: Duration): Future[AsyncResponse] = {
-    val requestWithAuth: Request = withAuthorization(request, makeAuthorization(request))
+    var byteBufToRelease: Option[ByteBuf] = None
+
+    // Can we retry this request if we get a 401 Unauthorized?
+    val canRetryRequest: Boolean = request match {
+      case full : FullRequest  => full.buf.retain(); byteBufToRelease = Option(full.buf); true // We need to retain() the ByteBuf to prevent a double free
+      case async: AsyncRequest => false // Cannot retry due to not having access to the request body
+      case file : FileRequest  => true
+    }
+
+    val originalAuthorization: Option[String] = makeAuthorization(request)
+    val requestWithAuth: Request = withAuthorization(request, originalAuthorization)
     
-    super.execute(requestWithAuth, timeout) flatMap { response: AsyncResponse =>
+    val res: Future[AsyncResponse] = super.execute(requestWithAuth, timeout) flatMap { response: AsyncResponse =>
       if (response.status.isUnauthorized) {
-        makeAuthorization(request, response) match {
-          case None    => Future.successful(response) // No auth available so return original response
-          case Some(a) =>
-            response.close() // Close out the original response
-            super.execute(withAuthorization(request, a), timeout) // Retry with auth
+        val updatedAuthorization: Option[String] = makeAuthorization(request, response)
+
+        if (!canRetryRequest || updatedAuthorization.isEmpty || updatedAuthorization === originalAuthorization) {
+          // There is no authorization value or it matches the original value in which case
+          // we would not expect a different response from the server.  So we just return
+          // the original response.
+          Future.successful(response)
+        } else {
+          // Close out the original response
+          response.close()
+
+          // Retry with the updated authorization -- Note: This code path is for DIGEST auth and not needed for BASIC
+          super.execute(withAuthorization(request, updatedAuthorization), timeout)
         }
       } else {
         // Return original response
         Future.successful(response)
       }
     }
+
+    // If this was a FullRequest we need to make sure we release the request ByteBuf when we are complete
+    if (byteBufToRelease.isDefined) res.onComplete{ _ => byteBufToRelease.get.release() }
+
+    res
   }
   
   private def withAuthorization(request: Request, auth: Option[String]): Request = {    
